e7982e74a830dbb08106d1c8cfadf901cdac6163
Titir88 <146040840+Titir88@users.noreply.github.com>
2026-07-17 02:56:37 -0400

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import uvicorn
-import os
-from app_full import app
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8402)))
